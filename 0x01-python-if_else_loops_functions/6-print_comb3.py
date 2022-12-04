9a6f5feb93fc3875a3361e6e27e34aa10b51a10a
Iconart <okequadri43@gmail.com>
2022-12-04 10:31:30 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,4 @@ for num1 in range(0, 9):
             if (num1 == 8) and (num2 == 9):
                 print("{}{}".format(num1, num2))
             else:
-                print("{}{}".format(num1, num2),
-                    end=", ")
+                print("{}{}".format(num1, num2), end=", ")
