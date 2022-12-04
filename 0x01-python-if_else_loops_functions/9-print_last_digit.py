3fb22790db09e7a66a033e5278433c749dac5f76
Iconart <okequadri43@gmail.com>
2022-12-04 21:27:09 +0000

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = number % 10
-    return(last)
+    if number > 0:
+        number = -number
+    last_digit = number % 10
+    print(last_digit, end="")
+
+    return (last_digit)
