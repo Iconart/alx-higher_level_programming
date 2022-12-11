49c78abd6a847043cec3c6b2047c9107691088eb
Iconart <okequadri43@gmail.com>
2022-12-11 02:43:34 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         first = None
+        return (first)
     length = len(sentence)
     first = sentence[0]
     return (length, first)
