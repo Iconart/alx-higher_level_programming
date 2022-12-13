b22845ae7405a79fc3360925be4547fe9985c486
Iconart <okequadri43@gmail.com>
2022-12-13 14:19:48 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 def square_matrix_simple(matrix=[]):
     return(list(map(lambda row: list(map(
-        lambda column: column ** 2, row)), matrix)))
+    lambda column: column ** 2, row)), matrix)))
