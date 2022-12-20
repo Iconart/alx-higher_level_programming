a1fe8415341de67be050142c198f137dd9664c8b
Iconart <okequadri43@gmail.com>
2022-12-20 14:38:56 +0000

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
-        raise ValueError from None
+    except (ValueError, TypeError):
         return False
