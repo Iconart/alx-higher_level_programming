b41fa5cb0ad893c961318633a25507cc337475d9
Iconart <okequadri43@gmail.com>
2022-12-22 01:29:01 +0000

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,20 +6,4 @@ def safe_print_integer_err(value):
         return True
     except Exception as e:
         print("Exception: ", e, file=sys.stderr)
-    return False
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-        
-
+    return False 
