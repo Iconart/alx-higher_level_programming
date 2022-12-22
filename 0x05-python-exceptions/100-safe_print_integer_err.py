735861f48348d67baf1b3544ba9eccb80eff64ff
Iconart <okequadri43@gmail.com>
2022-12-22 01:50:55 +0000

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,7 @@ def safe_print_integer_err(value):
     try:
     	print("{:d}".format(value))
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        oke = file=sys.stderr
+        print("Exception: {}".format(e), oke)
         return False
     return True
