f96617d845473a9391144eb67a5a860e64a10b1c
Iconart <okequadri43@gmail.com>
2022-12-23 21:43:06 +0000

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,14 +4,11 @@
 
 class Square:
     """Define square"""
-
     def __init__(self, size=0):
         """checking for some conditions to be meant"""
-
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
