273f729d064fe6f4aad134529b1b78c0112b4464
Iconart <okequadri43@gmail.com>
2022-12-23 22:01:36 +0000

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,4 +17,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+        """Method for area"""
+
+
         return self.__size ** 2
