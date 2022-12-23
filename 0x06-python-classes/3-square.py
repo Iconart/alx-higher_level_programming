d6fc6a32cecc49db9dafc42ea7afb79de67952ae
Iconart <okequadri43@gmail.com>
2022-12-23 22:03:53 +0000

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,19 +5,15 @@ size to produce Area"""
 
 class Square:
     """Define a square"""
-
-
     def __init__(self, size=0):
         """the value of size should be integer"""
-
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Method for area"""
-
-
         return self.__size ** 2
