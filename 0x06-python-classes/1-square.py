8491c96b46d7fa140f5a9882cdc134a4f57a8ebe
Iconart <okequadri43@gmail.com>
2022-12-23 18:15:04 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Square module"""
+"""square module"""
 
 class Square:
-    """Defines a square"""
-
+    """Define a square"""
 
     def __init__(self, size):
         self.__size = size
