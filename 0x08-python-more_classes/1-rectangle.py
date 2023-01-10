79c1d8f703ad305db3debdb1500c6c948ec76411
Iconart <okequadri43@gmail.com>
2023-01-10 19:06:15 +0000

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,16 +3,20 @@
 
 class Rectangle:
     """Rectangle class"""
+
     def __init__(self, width=0, height=0):
+        """Initialise width and heigt"""
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
+        """Retrive the width"""
         return self.__width
         
     @width.setter
     def width(self, value):
+        """Set the width to value"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -22,10 +26,12 @@ class Rectangle:
             
     @property
     def height(self):
+        """Retrieve the height"""
         return self.__height
         
     @height.setter
     def height(self, value):
+        """Set the height to value"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
